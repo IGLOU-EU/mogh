025981a942925d2b992e09618df935e87398c695
Adrien Kara <adrien@iglou.eu>
2022-08-24 01:09:09 +0200

    🐛 remove useless colored output from install script
    Signed-off-by: Adrien Kara <adrien@iglou.eu>

--- tools/install.sh
+++ tools/install.sh
@@ -22,32 +22,32 @@ INSTALL_PATH+="/hooks"
 
 # check if the install path already exists
 if [[ -d $INSTALL_PATH ]]; then
-    echo "\e[32m[INFO]\e[0m The install path already exists and it will be moved to ${INSTALL_PATH}.old"
+    echo "[INFO] The install path already exists and it will be moved to ${INSTALL_PATH}.old"
     mv "$INSTALL_PATH" "$INSTALL_PATH.old" || {
-        echo "\e[31m[ERROR]\e[0m Failed to move the install path" >&2
+        echo "[ERROR] Failed to move the install path" >&2
         exit 1
     }
 fi
 
 # create the install path
 mkdir -p "$INSTALL_PATH" || {
-    echo "\e[31m[ERROR]\e[0m Failed to create the install path" >&2
+    echo "[ERROR] Failed to create the install path" >&2
     exit 1
 }
 
 # clone to the install path
-echo "\e[32m[INFO]\e[0m Cloning to the install path"
+echo "[INFO] Cloning to the install path"
 git clone "$GIT_REPO" "$INSTALL_PATH" > /dev/null || {
-    echo "\e[31m[ERROR]\e[0m Failed to clone to the install path" >&2
+    echo "[ERROR] Failed to clone to the install path" >&2
     exit 1
 }
 
 # enable the templates in git config
-echo "\e[32m[INFO]\e[0m Enable the templates in git config"
+echo "[INFO] Enable the templates in git config"
 git config --global init.templatedir "$TEMPLATE_PATH" || {
-    echo "\e[31m[ERROR]\e[0m Failed to enable the templates in git config" >&2
+    echo "[ERROR] Failed to enable the templates in git config" >&2
     exit 1
 }
 
 # enjoy your new git hooks 🎉
-echo "\e[32m[INFO]\e[0m Installation complete"
+echo "[INFO] Installation complete 🎉"
